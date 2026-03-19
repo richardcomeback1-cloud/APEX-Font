ae672d6849f3bbfd37296c2a038cc66b427802c3
richardcomeback1-cloud <richardcomeback1@gmail.com>
2026-03-19 08:03:05 +0700

    Add remaining bundled font registrations

--- APEX-Font/config.lua
+++ APEX-Font/config.lua
@@ -1,9 +1,52 @@
 Config = {}
 
 Config.RegisterFont = {
-    srbn = {
+    sarabun = {
         registerFontFile = 'sarabun',
         registerFontId = 'Sarabun'
     },
-
+    kanit = {
+        registerFontFile = 'Kanit',
+        registerFontId = 'Kanit'
+    },
+    mitr = {
+        registerFontFile = 'Mitr',
+        registerFontId = 'Mitr'
+    },
+    serithai = {
+        registerFontFile = 'Serithai',
+        registerFontId = 'Serithai'
+    },
+    thaifont = {
+        registerFontFile = 'ThaiFont',
+        registerFontId = 'ThaiFont'
+    },
+    thaifont1 = {
+        registerFontFile = 'ThaiFont1',
+        registerFontId = 'ThaiFont1'
+    },
+    athiti = {
+        registerFontFile = 'athiti',
+        registerFontId = 'Athiti'
+    },
+    comic = {
+        registerFontFile = 'comic',
+        registerFontId = 'Comic Sans MS'
+    },
+    dragonpixelth = {
+        registerFontFile = 'dragonpixelth',
+        registerFontId = 'Dragon Pixel TH'
+    },
+    font4thai = {
+        registerFontFile = 'font4thai',
+        registerFontId = 'Font 4 Thai'
+    },
+    arialnarrow = {
+        registerFontFile = 'ArialNarrow',
+        registerFontId = 'Arial Narrow'
+    },
+    uidsalmon2019 = {
+        registerFontFile = 'uidsalmon201 9',
+        registerFontId = 'UID SALMON 2019'
+    },
 }
